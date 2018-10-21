b1a9d3d3802a3533c4a6d91539cb651ab4ee5f2f
mpritzkoleit <maxpritzkoleit@aol.com>
2018-10-21 16:04:52 +0200

    bug fixes and improvements in DDPG

--- DDPG.py
+++ DDPG.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 from NeuralNetworkModels import Actor, Critic
 import random
-import timeit
+import time
 
 class DDPG(Algorithm):
     """ Deep Deterministic Policy Gradient - Implementation based on PyTorch (https://pytorch.org)
@@ -30,6 +30,7 @@ class DDPG(Algorithm):
         self.R = DataSet(nData)
         self.episode = 0
         self.plotEpisode = plotEpisode
+        self.epsDecay = 1
 
     def run_episode(self):
         print('started episode ', self.episode)
@@ -41,31 +42,22 @@ class DDPG(Algorithm):
         self.agent.reset()
         for _ in tt:
             # agent computes control/action
-            if self.episode > 0:
-                u = self.agent.take_action(self.dt, self.environment.x)
-            else:
-                u = self.agent.take_random_action(self.dt)
+            u = self.agent.take_random_action(self.dt, self.environment.x)
+
             # simulation of environment
             c = self.environment.step(self.dt, u)
-
             cost.append(c)
 
             # store transition in dataset (x_, u, x, c)
-            transition = ({'x_': self.environment.x_, 'u': self.agent.u, 'x': self.environment.x, 'c': c})
+            transition = ({'x_': self.environment.x_, 'u': self.agent.u, 'x': self.environment.x, 'c': [c]})
             self.R.add_sample(transition)
             if self.R.length > 64 and self.episode > 0:
                 self.agent.training(self.R)
-
             if self.environment.terminated:
                 print('Environment terminated!')
                 break
-
         # store the mean of the incremental cost
         self.meanCost.append(np.mean(cost))
-        print('Mean cost: ',np.mean(cost))
-        # learning
-        self.agent.training(self.R)
-
         self.episode += 1
 
         pass
@@ -80,9 +72,9 @@ class DDPG(Algorithm):
             if k % 10 == 0:
                 self.learning_curve()
                 # if self.meanCost[-1] < 0.01: # goal reached
-            if k % self.plotEpisode == 0.:
+            if k % self.plotEpisode == 0:
                 self.plot()
-                self.animation()
+                #self.animation()
 
 
     def plot(self):
@@ -120,7 +112,7 @@ class ActorCritic(Agent):
         eps (float [0, 1]): with probability eps a random action/control is returned
     """
 
-    def __init__(self, xDim, uDim, uMax, eps=0.0, gamma=0.99, tau=0.001):
+    def __init__(self, xDim, uDim, uMax, gamma=0.99, tau=0.001):
         super(ActorCritic, self).__init__(1)
         self.xDim = xDim
         self.uDim = uDim
@@ -131,46 +123,42 @@ class ActorCritic(Agent):
         self.critic1 = Critic(xDim, uDim)
         self.critic2 = Critic(xDim, uDim)
         self.blend_hard(self.critic1, self.critic2)
-        self.eps = eps
         self.gamma = gamma
         self.tau = tau
         self.optimCritic = torch.optim.Adam(self.critic1.parameters(), lr=1e-3, weight_decay=1e-2)
         self.optimActor = torch.optim.Adam(self.actor1.parameters(), lr=1e-4)
-
+        self.noise = OUnoise(uDim)
 
     def training(self, dataSet):
         # loss function (mean squared error)
         criterion = nn.MSELoss()
 
         # training data
-        xInputs, uInputs, qTargets = self.training_data(dataSet)
-
+        x_Inputs, uInputs, qTargets = self.training_data(dataSet)
         # eval mode on (batch normalization)
         for epoch in range(1):  # loop over the dataset multiple times
             running_loss = 0.0
-            qOutputs = self.critic1(xInputs, uInputs)
-            muOutputs = self.actor1(xInputs)
-
-            # backward + optimize
-            lossCritic = criterion(qOutputs, qTargets.detach())
-            lossActor = self.critic1(xInputs, muOutputs).mean() # -self.critic1(xInputs, muOutputs).mean() when using rewards instead of cost
-
+            qOutputs = self.critic1(x_Inputs, uInputs)
+            qOutputs = torch.squeeze(qOutputs)
+            muOutputs = self.actor1(x_Inputs)
             self.train()
-            # zero the parameter gradients
-            self.optimCritic.zero_grad()
-            self.optimActor.zero_grad()
+            # backward + optimize
+            lossCritic = criterion(qOutputs, qTargets)
+            lossActor = -self.critic1(x_Inputs, muOutputs).mean() # -self.critic1(xInputs, muOutputs).mean() when using rewards instead of cost
 
             #loss.backward(retain_graph=True)
+            self.optimCritic.zero_grad()
             lossCritic.backward()
-            lossActor.backward()
-
             self.optimCritic.step()
+
+            self.optimActor.zero_grad()
+            lossActor.backward()
             self.optimActor.step()
-            self.eval()
+
             self.blend(self.critic1, self.critic2)
             self.blend(self.actor1, self.actor2)
-
             # eval mode on (batch normalization)
+            self.eval()
         pass
 
     def train(self):
@@ -187,7 +175,7 @@ class ActorCritic(Agent):
 
     def blend(self, source, target):
         for wTarget, wSource in zip(target.parameters(), source.parameters()):
-            wTarget.data.copy_(self.tau*wSource.data + (1 - self.tau)*wTarget.data)
+            wTarget.data.copy_(self.tau*wSource.data + (1.0 - self.tau)*wTarget.data)
         return
 
     def blend_hard(self, source, target):
@@ -197,45 +185,37 @@ class ActorCritic(Agent):
 
     def take_action(self, dt, x):
         """ eps-greedy policy """
-        if random.uniform(0, 1) > self.eps:
-            # greedy control/action
-            x = torch.Tensor([x])
-            self.u = np.asarray(self.actor1(x).detach())[0]
-            self.history = np.concatenate((self.history, np.array([self.u])))  # save current action in history
-            self.tt.extend([self.tt[-1] + dt])  # increment simulation time
-        else:
-            self.take_random_action(dt)
-
+        # greedy control/action
+        self.eval()
+        x = torch.Tensor([x])
+        self.u = np.asarray(self.actor1(x).detach())[0]
+        # self.u = np.clip(np.asarray(self.actor1(x).detach())[0] + self.noise()*self.uMax, -self.uMax, self.uMax)
+        self.history = np.concatenate((self.history, np.array([self.u])))  # save current action in history
+        self.tt.extend([self.tt[-1] + dt])  # increment simulation time
         return self.u
 
-    def take_random_action(self, dt):
+    def take_random_action(self, dt, x):
         """ random policy """
-        self.u = [np.random.uniform(-self.uMax, self.uMax)]
+        #self.u = [np.random.uniform(-self.uMax, self.uMax)]
+        self.eval()
+        x = torch.Tensor([x])
+        u = np.asarray(self.actor1(x).detach())[0] + self.noise.sample()*self.uMax
+        self.u = np.clip(u, -self.uMax, self.uMax)
         self.history = np.concatenate((self.history, np.array([self.u])))  # save current action in history
         self.tt.extend([self.tt[-1] + dt])  # increment simulation time
         return self.u
 
     def training_data(self, dataSet):
         # eval mode on (batch normalization)
-        self.actor1.eval()
-        self.actor2.eval()
-        self.critic1.eval()
-        self.critic2.eval()
-
         batch = dataSet.minibatch(64)
-        xInputs = torch.Tensor(len(batch), self.xDim)
-        uInputs = torch.Tensor(len(batch), self.uDim)
-        qTargets = torch.Tensor(len(batch), 1)
-        for i in range(len(batch)):
-            x_ = torch.Tensor(batch[i]['x_'])
-            u = torch.Tensor(batch[i]['u'])
-            x = torch.Tensor([batch[i]['x']])
-            c = torch.Tensor([batch[i]['c']])
-            xInputs[i, :] = x_
-            uInputs[i, :] = u
-            qTargets[i, :] = c + self.gamma*self.critic2(x, self.actor2(x))
-
-        return xInputs, uInputs, qTargets
+        x_Inputs = torch.Tensor([sample['x_'] for sample in batch])
+        xInputs =  torch.Tensor([sample['x'] for sample in batch])
+        uInputs =  torch.Tensor([sample['u'] for sample in batch])
+        costs =  torch.Tensor([sample['c'] for sample in batch])
+        self.eval()
+        qTargets = costs + self.gamma*self.critic2(xInputs, self.actor2(xInputs)).detach()
+        qTargets = torch.squeeze(qTargets)
+        return x_Inputs, uInputs, qTargets
 
     def plot(self):
         """ Plots the agents history
@@ -261,7 +241,7 @@ class ActionNoise(object):
 
 # class OrnsteinUhlenbeckProcess(object):
 class OrnsteinUhlenbeckActionNoise(ActionNoise):
-    def __init__(self, mu, sigma, theta=.15, dt=1e-2, x0=None):
+    def __init__(self, mu, sigma, theta=.15, dt=5e-2, x0=None):
         self.theta = theta
         self.mu = mu
         self.sigma = sigma
@@ -279,3 +259,20 @@ class OrnsteinUhlenbeckActionNoise(ActionNoise):
 
     def __repr__(self):
         return 'OrnsteinUhlenbeckActionNoise(mu={}, sigma={})'.format(self.mu, self.sigma)
+
+class OUnoise:
+    def __init__(self, action_dim, mu=0, theta=0.15, sigma=0.2):
+        self.action_dim = action_dim
+        self.mu = mu
+        self.theta = theta
+        self.sigma = sigma
+        self.X = np.ones(self.action_dim) * self.mu
+
+    def reset(self):
+        self.X = np.ones(self.action_dim) * self.mu
+
+    def sample(self):
+        dx = self.theta * (self.mu - self.X)
+        dx = dx + self.sigma * np.random.randn(len(self.X))
+        self.X = self.X + dx
+        return self.X
--- NeuralNetworkModels.py
+++ NeuralNetworkModels.py
@@ -46,24 +46,29 @@ class Critic(nn.Module):
         super(Critic, self).__init__()
         self.xDim = xDim
         self.uDim = uDim
+        self.layer0_bn = nn.BatchNorm1d(xDim)
         self.layer1 = nn.Linear(xDim, 400)
         self.layer1_bn = nn.BatchNorm1d(400)
-        self.layer21 = nn.Linear(400+uDim, 300)
-        self.layer22 = nn.Linear(uDim, 300, bias=False) # Ax+Bu+b
+        self.layer2 = nn.Linear(400 + uDim, 300)
         self.layer3 = nn.Linear(300, 1)
 
         # weight initialization
-        wMin = -3*10e-3
-        wMax = 3*10e-3
+        wMin = -3.0*1e-3
+        wMax = 3.0*1e-3
         self.fanin_init(self.layer1)
-        self.fanin_init(self.layer21)
+        self.fanin_init(self.layer2)
         self.layer3.weight = torch.nn.init.uniform_(self.layer3.weight, a=wMin, b=wMax)
         self.layer3.bias = torch.nn.init.uniform_(self.layer3.bias, a=wMin, b=wMax)
 
     def forward(self, x, u):
         # connect layers
-        h1 = F.relu(self.layer1_bn(self.layer1(x)))
-        h2 = F.relu(self.layer21(torch.cat((h1, u), 1))) #+ self.layer22(u))
+        x = self.layer0_bn(x)
+        h1 = self.layer1_bn(self.layer1(x))
+        h1 = self.layer1(x)
+        h1 = F.relu(h1)
+        h2 = torch.cat((h1, u), 1)
+        h2 = self.layer2(h2)
+        h2 = F.relu(h2)
         y = F.relu(self.layer3(h2))
         return y
 
@@ -90,6 +95,7 @@ class Actor(nn.Module):
         self.xDim = xDim
         self.uDim = uDim
         self.uMax = uMax
+        self.layer0_bn = nn.BatchNorm1d(xDim)
         self.layer1 = nn.Linear(xDim, 400)
         self.layer1_bn = nn.BatchNorm1d(400)
         self.layer2 = nn.Linear(400, 300)
@@ -97,18 +103,21 @@ class Actor(nn.Module):
         self.layer3 = nn.Linear(300, uDim)
 
         # weight initialization
-        wMin = -3*10e-3
-        wMax = 3*10e-3
+        wMin = -3*1e-3
+        wMax = 3*1e-3
         self.fanin_init(self.layer1)
         self.fanin_init(self.layer2)
         self.layer3.weight = torch.nn.init.uniform_(self.layer3.weight, a=wMin, b=wMax)
         self.layer3.bias = torch.nn.init.uniform_(self.layer3.bias, a=wMin, b=wMax)
 
     def forward(self, x):
-        # connect layers
-        h1 = F.relu(self.layer1_bn(self.layer1(x)))
-        h2 = F.relu(self.layer2_bn(self.layer2(h1)))
-        y = F.tanh(self.layer3(h2))
+        x_bn = self.layer0_bn(x)
+        h1_bn = self.layer1_bn(self.layer1(x_bn))
+        h1 = F.relu(h1_bn)
+        h2_bn = self.layer2_bn(self.layer2(h1))
+        h2 = F.relu(h2_bn)
+        h3 = self.layer3(h2)
+        y = F.tanh(h3)
         y = torch.mul(y, self.uMax) # scale output
         return y
 
