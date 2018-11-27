8ca4fbe51b8a41d256710274856fe797ff1b6bf9
mpritzkoleit <maxpritzkoleit@aol.com>
2018-11-27 13:25:10 +0100

    add NMPC agent frame

--- Agents.py
+++ Agents.py
@@ -73,3 +73,58 @@ class FeedBack(Agent):
 
         return fig, ax
 
+
+class NMPC(Agent):
+    """Agent subclass: a NMPC implementation based on
+
+        L. Grüne, Nonlinear Model Predictive Control
+
+        Attributes:
+            mu (function): feedback law
+            tt (array):
+            history (array): previous states (x[0],x[1],...,x[k-1])
+            tt (list): time vector (corresponding to history)
+
+        """
+
+    def __init__(self, mu, m):
+        super(MPC, self).__init__(m)
+        self.mu = mu
+
+    def take_action(self, dt, x):
+        """ Computes control/action of the agent
+
+        Args:
+            dt (int, float): duration of step (not solver step size)
+            x (array): state
+
+        Returns:
+            u (array): control/action
+
+        """
+
+        self.u = self.mu(x)  # compute control signal
+        self.history = np.concatenate((self.history, np.array([self.u])))  # save current action in history
+        self.tt.extend([self.tt[-1] + dt])  # increment simulation time
+
+        return self.u
+
+    def plot(self):
+        """ Plots the agents history (the control trajectory)
+
+        Returns:
+            fig (matplotlib.pyplot.figure)
+
+        """
+
+        fig, ax = plt.subplots(len(self.u), 1, sharex='col')
+        # Plot control trajectories
+        fig.suptitle('Controls')
+        for i in range(len(self.u)):
+            ax[i].step(self.tt, self.history[:, i], label=r'$u_'+str(i+1)+'$')
+            ax[i].grid(True)
+            ax[i].legend(loc='upper right')
+        plt.xlabel('t in s')
+
+        return fig, ax
+
