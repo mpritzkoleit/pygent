d6dfe67062eb23efe1ff5608258c70bd0d96a5ff
mpritzkoleit <maxpritzkoleit@aol.com>
2019-02-22 18:10:15 +0100

    iLQR: implemented constrained control via QP

--- iLQR.py
+++ iLQR.py
@@ -10,9 +10,15 @@ import random
 import time
 from Utilities import nth_derivative, torch_jacobian, hessian, hessian2, hessian3, c2d, unpackBlockMatrix, system_linearization
 import sympy as sp
+try:
+    from sympy_to_c import sympy_to_c as sp2c
+except ImportError:
+    print('sympy-to-c could not be imported!')
+
 import os
 import cvxopt as opt
 opt.solvers.options['show_progress'] = False
+
 class iLQR(Algorithm):
     """ iLQR -
 
@@ -54,20 +60,21 @@ class iLQR(Algorithm):
         self.init_trajectory()
         self.max_iters = maxIters
         self.mu_min = 1e-6
-        self.mu_max = 1e6
-        self.mu_d0 = 1.5
+        self.mu_max = 1e10
+        self.mu_d0 = 1.6
         self.mu_d = 1.
-        self.mu = 1.0
+        self.mu = 1.
         self.alphas = 10**np.linspace(0, -3, 11)
         self.zmin = 0.
         self.tolGrad = tolGrad
         self.tolFun = tolFun
         self.constrained = constrained
         self.lims = self.environment.uMax
+        self.regType = 2.
 
 
     def cost_fnc(self, x, u):
-        c = self.environment.cost(x, u, None)
+        c = self.environment.cost(x, u, None)*self.dt
         return c
 
 
@@ -80,17 +87,17 @@ class iLQR(Algorithm):
 
         cost = 0
         for i in range(self.steps):
-            u = (np.matmul(self.KK[i], (self.environment.x - xx_[i])) + alpha * self.kk[i] + uu_[i])[0]
-
+            u = self.KK[i]@(self.environment.x - xx_[i]) + alpha*self.kk[i].T[0] + uu_[i]
+            if self.constrained:
+                u = np.clip(u, -self.environment.uMax, self.environment.uMax)
             self.agent.control(self.dt, u)
 
             if self.fastForward:
                 c = self.environment.fast_step(self.dt, u)
             else:
                 c = self.environment.step(self.dt, u)
-
             cost += c
-
+        cost += self.fcost_fnc(self.environment.x)*self.dt
         xx = self.environment.history
         uu = self.agent.history[1:]
 
@@ -99,10 +106,12 @@ class iLQR(Algorithm):
 
     def backward_pass(self):
         x = self.xx[-1]
-        system_matrices = self.sys_lin()
+        system_matrices = [self.linearization(x, u) for x, u in zip(self.xx[:-1], self.uu)]
+        cost_matrices = [self.cost_lin(x, u) for x, u in zip(self.xx[:-1], self.uu)]
         # DARE in Vt, vt
-        Vt = self.Cfxx(*x)
-        vt = self.cfx(*x)
+        vx = self.cfx(x)*self.dt
+        Vxx = self.Cfxx(x)*self.dt
+
         dV1 = np.zeros((1,1))
         dV2 = np.zeros((1,1))
 
@@ -118,53 +127,65 @@ class iLQR(Algorithm):
             x = self.xx[i]
             u = self.uu[i]
 
-            Cxx, Cuu, Cxu, cx, cu = self.cost_lin(x, u)
+            # expanded cost
+            Cxx, Cuu, Cxu, cx, cu = cost_matrices[i]#self.cost_lin(x, u)
 
-            ct = np.block([[cx], [cu]])
+            # expanded system dynamics
+            Ad, Bd, ft = system_matrices[i]
+            # eq. 5a/b
+            qx = cx + np.matmul(Ad.T, vx) #+ np.matmul(Ad.T, np.matmul(Vt, ft))
+            qu = cu + np.matmul(Bd.T, vx) # +np.matmul(Bd.T, np.matmul(Vt, ft))
 
-            Ct = np.block([[Cxx, Cxu], [Cxu.T, Cuu]])
+            #eq 5c-e
+            Qxx = Cxx + np.matmul(Ad.T, np.matmul(Vxx, Ad))
+            Quu = Cuu + np.matmul(Bd.T, np.matmul(Vxx, Bd))
+            Qux = Cxu.T + np.matmul(Bd.T, np.matmul(Vxx, Ad))
 
-            # Linerisierung
-            Ft, ft = system_matrices[i]
+            VxxReg = Vxx + self.mu*np.eye(self.xDim)*(self.regType == 1)
+            QuxReg = Cxu.T + np.matmul(Bd.T, np.matmul(VxxReg, Ad))
+            QuuReg = Cuu + np.matmul(Bd.T, np.matmul(VxxReg, Bd)) + self.mu*np.eye(self.uDim)*(self.regType == 2)
 
-            Qt = Ct + np.matmul(Ft.T, np.matmul(Vt + self.mu * np.eye(self.xDim), Ft))
-
-            Qxx, Qxu, Qux, Quu = unpackBlockMatrix(Qt, self.xDim, self.uDim)
-
-            qt = ct + np.matmul(Ft.T, np.matmul(Vt, ft)) + np.matmul(Ft.T, vt)
-            qx = qt[:self.xDim]
-            qu = qt[self.xDim:]
-
-            try:
-                np.linalg.cholesky(Quu)
-
-            except np.linalg.LinAlgError as e:
-                success = False
-                break
-            QuuReg = Quu + self.mu * np.eye(self.uDim)
-            QuuInv = np.linalg.inv(QuuReg)
             if self.constrained: #  solve QP
-                QuuRegOpt = opt.matrix(QuuReg)
+                QuuOpt = opt.matrix(QuuReg)
                 quOpt = opt.matrix(qu)
-                G = opt.matrix(np.kron(np.array([[1.], [-1.]]), np.eye(self.uDim)))
-                h = opt.matrix(np.array([self.lims-u, self.lims+u]).reshape((2*self.uDim,)))
-
-                sol = opt.solvers.qp(QuuRegOpt, quOpt, G, h)
+                G = np.kron(np.array([[1.], [-1.]]), np.eye(self.uDim))
+                h = np.array([self.lims-u, self.lims+u]).reshape((2*self.uDim,))
+                GOpt = opt.matrix(G)
+                hOpt = opt.matrix(h)
+                sol = opt.solvers.qp(QuuOpt, quOpt, GOpt, hOpt)
                 kt = np.array(sol['x'])
-                kt_ = -np.matmul(QuuInv, qu) # unconstrained case
-                # determine free controls, that are not clamped
-                clamped = np.isclose(kt, kt_)[:,0] == False
-                Kt = -np.matmul(QuuInv, Qux)
-                Kt[clamped, :] = 0.
+                clamped = np.zeros((self.uDim), dtype=bool)
+                # adapted from boxQP.m of iLQG package
+                uplims = np.isclose(kt.reshape(self.uDim, ), h[0:self.uDim], atol=1e-3)
+                lowlims = np.isclose(kt.reshape(self.uDim, ), h[self.uDim::], atol=1e-3)
+
+                clamped[uplims] = True
+                clamped[lowlims] = True
+                free_controls = np.logical_not(clamped)
+                Kt = np.zeros((self.uDim, self.xDim))
+                if any(free_controls):
+                    Kt[free_controls, :] = -np.linalg.solve(QuuReg, QuxReg)[free_controls, :]#-np.matmul(QuuInv, QuxReg)
             else:
-                kt = -np.matmul(QuuInv, qu)
-                Kt = -np.matmul(QuuInv, Qux)
+                try:
+                    np.linalg.cholesky(QuuReg)
 
-            Vt = Qxx + np.matmul(Kt.T, np.matmul(Quu, Kt)) + np.matmul(Kt.T, Qux) + np.matmul(Qux.T, Kt)
-            vt = qx + np.matmul(Kt.T, np.matmul(Quu, kt)) + np.matmul(Kt.T, qu) + np.matmul(Qux.T, kt)
+                except np.linalg.LinAlgError as e:
+                    print('Quu not positive-semidefinite')
+                    success = False
+                    break
 
-            dV2 = 0.5 * np.matmul(kt.T, np.matmul(Quu, kt))
-            dV1 = np.matmul(kt.T, qu)
+                kt = -np.linalg.solve(QuuReg, qu)#-np.matmul(QuuInv, qu)#
+                Kt = -np.linalg.solve(QuuReg, QuxReg)#-np.matmul(QuuInv, Qux)
+
+            vx = qx + Kt.T@Quu@kt + Kt.T@qu + Qux.T@kt
+            Vxx = Qxx + Kt.T@Quu@Kt + Kt.T@Qux + Qux.T@Kt
+            Vxx = 0.5*(Vxx + Vxx.T) # remain symmetry
+
+
+            #dV2 = 0.5 * np.matmul(kt.T, np.matmul(QuuReg, kt))
+            #dV1 = np.matmul(kt.T, qu)
+            dV2 = 0.5*kt.T@Quu@kt
+            dV1 = kt.T@qu
             # save Kt, kt
             self.KK.insert(0, Kt)
             self.kk.insert(0, kt)
@@ -189,51 +210,44 @@ class iLQR(Algorithm):
         success_gradient = False
         # later run_episode
         for _ in range(self.max_iters):
-            try:
-                success_bw = False
-                while not success_bw:
-                    V1, V2, success_bw = self.backward_pass()
-                    if not success_bw:
-                        # print('Backward successfull')
-                        print('diverged')
-                        # increase mu
-                        self.increase_mu()
-                        break
+            success_bw = False
+            while not success_bw:
+                V1, V2, success_bw = self.backward_pass()
+                if not success_bw:
+                    # print('Backward successfull')
+                    print('diverged')
+                    # increase mu
+                    self.increase_mu()
+                    break
+
+            success_fw = False
+            if success_bw:
                 # check for gradient
-                g_norm = np.mean(np.max(np.abs(np.array(self.kk).reshape((len(self.kk), self.uDim))/(np.abs(self.uu) + 1)), axis=0))
+                g_norm = np.mean(
+                    np.max(np.abs(np.array(self.kk).reshape((len(self.kk), self.uDim)) / (np.abs(self.uu) + 1)),
+                           axis=0))
                 if g_norm < self.tolGrad and self.mu < 1e-5:
+                    self.decrease_mu()
                     success_gradient = True
 
-                success_fw = False
-                if success_bw:
-                    # Line-search
-                    for alpha in self.alphas:
-                        xx, uu, cost = self.forward_pass(alpha)
-                        dcost = self.cost - cost
-                        expected = -alpha * (np.sum(V1) + alpha * np.sum(V2))
-                        if expected > 0:
-                            z = dcost / expected
-                        else:
-                            z = np.sign(dcost)
-                            print('non-positive expected reduction')
-                        # print('z :', z)
-                        if z > self.zmin:
-                            success_fw = True
-                            # print('Forward successfull')
-                            break
-                        # print('Forward not successfull')
-                    if not success_fw:
-                        # increase mu
-                        self.increase_mu()
-                        print('Forward not successfull')
-                print('Iteration ', _, '/ Cost: ', cost)
-            except KeyboardInterrupt:
-                self.plot()
-                plt.show()
-                self.animation()
-
+                # Line-search
+                for alpha in self.alphas:
+                    xx, uu, cost = self.forward_pass(alpha)
+                    dcost = self.cost - cost
+                    expected = -alpha * (np.sum(V1) + alpha * np.sum(V2))
+                    if expected > 0.:
+                        z = dcost / expected
+                    else:
+                        z = np.sign(dcost)
+                        print('non-positive expected reduction')
+                    # print('z :', z)
+                    if z > self.zmin:
+                        success_fw = True
+                        # print('Forward successfull')
+                        break
+                    # print('Forward not successfull')
             if success_fw:
-                # print('Iteration successfull')
+                print('Iter. ', _+1, '| Cost: %.7f' % self.cost,' | red.: %.5f' % dcost, '| exp.: %.5f' % expected)
                 self.cost = np.copy(cost)
                 self.xx = np.copy(xx)
                 self.uu = np.copy(uu)
@@ -242,11 +256,16 @@ class iLQR(Algorithm):
                 self.decrease_mu()
 
                 if dcost < self.tolFun:
-                    # Todo: should also stop if forward pass not succes
                     print('converged: small improvement')
                     break
                 if success_gradient:
                     print('converged: small gradient')
+                    break
+            else:
+                self.increase_mu()
+                print('Iter. ', _, '| Forward not successfull')
+                if self.mu > self.mu_max:
+                    print('converged: no improvement')
                     break
         print('Iterations ', _, '/ Final Cost-to-Go: ', self.cost)
         self.save()
@@ -256,7 +275,7 @@ class iLQR(Algorithm):
     def init_trajectory(self):
         # random trajectory
         for _ in range(self.steps):
-            u = np.random.uniform(0.1, 0.1, self.uDim)
+            u = np.random.uniform(0., 0., self.uDim)
             u = self.agent.control(self.dt, u)
             # necessary to store control in agents history
             if self.fastForward:
@@ -264,25 +283,22 @@ class iLQR(Algorithm):
             else:
                 c = self.environment.step(self.dt, u)
             self.cost += c
+        self.cost += self.fcost_fnc(self.environment.x)*self.dt
         self.xx = self.environment.history
         self.uu = self.agent.history[1:]
         pass
-
-    def sys_lin(self):
-        # 1st order taylor expansion of the system dynamics along a trajectory
-        return [self.linearization(x, u) for x, u in zip(self.xx, self.uu)]
 
     def linearization(self, x, u):
 
         A, B = system_linearization(lambda xx, uu: self.environment.ode(None, xx, uu), x, u)
 
-        Ad, Bd = c2d(A, B, self.dt)
-        #Ad = A*self.dt + np.eye(self.xDim)
-        #Bd = B*self.dt
+        #Ad, Bd = c2d(A, B, self.dt)
+        Ad = A*self.dt + np.eye(self.xDim)
+        Bd = B*self.dt
         #Ft = np.block([[self.Ad(x, u), self.Bd(x, u)]])
-        Ft = np.block([[Ad, Bd]])
-        ft = np.zeros((self.xDim,1))#self.dt *0* np.expand_dims(self.environment.ode(None, x, u), 0).T
-        return Ft, ft
+        #Ft = np.block([[Ad, Bd]])
+        fd = np.zeros((self.xDim,1))#self.dt *0* np.expand_dims(self.environment.ode(None, x, u), 0).T
+        return Ad, Bd, fd
 
     def cost_init(self):
         # 2nd order taylor expansion of the cost function along a trajectory
@@ -305,13 +321,23 @@ class iLQR(Algorithm):
         cfx = ccf.jacobian(xx)
         Cfxx = cfx.jacobian(xx)
 
-        self.cx = sp.lambdify((xx, uu), cx.T)
-        self.cu = sp.lambdify((xx, uu), cu.T)
-        self.Cxx = sp.lambdify((xx, uu), Cxx)
-        self.Cuu = sp.lambdify((xx, uu), Cuu)
-        self.Cxu = sp.lambdify((xx, uu), Cxu)
-        self.cfx = sp.lambdify(xx, cfx.T)
-        self.Cfxx = sp.lambdify(xx, Cfxx)
+        try:
+            self.cx = sp2c.convert_to_c((xx, uu), cx.T, cfilepath="cx.c", use_exisiting_so=False)
+            self.cu = sp2c.convert_to_c((xx, uu), cu.T, cfilepath="cu.c", use_exisiting_so=False)
+            self.Cxx = sp2c.convert_to_c((xx, uu), Cxx, cfilepath="Cxx.c", use_exisiting_so=False)
+            self.Cuu = sp2c.convert_to_c((xx, uu), Cuu, cfilepath="Cuu.c", use_exisiting_so=False)
+            self.Cxu = sp2c.convert_to_c((xx, uu), Cxu, cfilepath="Cxu.c", use_exisiting_so=False)
+            self.cfx = sp2c.convert_to_c((xx,), cfx.T, cfilepath="cfx.c", use_exisiting_so=False)
+            self.Cfxx = sp2c.convert_to_c((xx,), Cfxx, cfilepath="Cfxx.c", use_exisiting_so=False)
+        except:
+            print('Could not use sympy-to-c cost functions!')
+            self.cx = sp.lambdify((xx, uu), cx.T)
+            self.cu = sp.lambdify((xx, uu), cu.T)
+            self.Cxx = sp.lambdify((xx, uu), Cxx)
+            self.Cuu = sp.lambdify((xx, uu), Cuu)
+            self.Cxu = sp.lambdify((xx, uu), Cxu)
+            self.cfx = sp.lambdify((xx,), cfx.T)
+            self.Cfxx = sp.lambdify((xx,), Cfxx)
 
         pass
 
@@ -327,6 +353,7 @@ class iLQR(Algorithm):
         self.Bd = sp.lambdify((xx, uu), Bd)
         pass
 
+
     def cost_lin(self, x, u):
         Cxx = self.Cxx(x, u)
         Cuu = self.Cuu(x, u)
@@ -336,11 +363,8 @@ class iLQR(Algorithm):
         return Cxx, Cuu, Cxu, cx, cu
 
     def decrease_mu(self):
-        self.mu_d = min(1 / self.mu_d0, self.mu_d / self.mu_d0)
-        if self.mu * self.mu_d > self.mu_max:
-            self.mu *= self.mu_d
-        elif self.mu * self.mu_d < self.mu_min:
-            self.mu = 0.0
+        self.mu_d = min(self.mu_d/self.mu_d0, 1/self.mu_d0)
+        self.mu = self.mu*self.mu_d*(self.mu > self.mu_min)
         pass
 
     def increase_mu(self):
@@ -351,12 +375,14 @@ class iLQR(Algorithm):
     def plot(self):
         self.environment.plot()
         plt.savefig(self.path+'plots/environment.pdf')
+        plt.savefig(self.path + 'plots/environment.pgf')
         self.agent.plot()
         plt.savefig(self.path+'plots/controller.pdf')
+        plt.savefig(self.path + 'plots/controller.pgf')
         plt.close('all')
 
     def animation(self):
-        ani = self.environment.animation(0, self.cost)
+        ani = self.environment.animation()
         if ani != None:
             ani.save(self.path+'animations/animation.mp4', fps=1/self.dt)
         plt.close('all')
