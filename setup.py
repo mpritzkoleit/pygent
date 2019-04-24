b1c1c412f1057ae5deb5e42a5c9ccf54d558fcdd
mpritzkoleit <maxpritzkoleit@aol.com>
2019-04-24 16:18:36 +0200

    update setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
 
+
+with open("requirements.txt") as requirements_file:
+    requirements = requirements_file.read()
+
 setup(
-    name='pygent',
-    version='',
+    name='PyGent',
+    version='0.1',
     packages=['', 'algorithms', 'modeling_scripts'],
-    url='',
-    license='',
+    url='https//github.com/mpritzkoleit/pygent',
     author='Max Pritzkoleit',
-    author_email='',
-    description=''
+    author_email='Max.Pritzkoleit@tu-dresden.de',
+    description = 'Python library for control and reinforcement learning',
+    long_description='PyGent is a reinforcement learning library, '
+                'that delivers a general framework, similar to OpenAIs "gym", for control and simulation of dynamic systems.'
+                'It implements some state-of-the-art deep reinforcement learning and '
+                'trajectory optimization algorithms.'
 )
