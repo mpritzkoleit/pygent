b675b26c8f670e5739e77b45f14330f5c1f34d10
mpritzkoleit <maxpritzkoleit@aol.com>
2019-05-08 11:04:54 +0200

    add MarBot and new example with Gym's Pendulum-v0

---
+++ examples/ddpg/marbot.py
@@ -0,0 +1,30 @@
+from environments import MarBot
+from algorithms.ddpg import DDPG
+import numpy as np
+
+
+def c_k(x, u, x_):
+    x1, x2, x3, x4 = x_
+    u1, = u
+    c = 8.7*x1**2 + 8.7*x2**2 + 10e-5*x3**2 + 10e-5*x4**2 + 4.7*u1**2
+    return c
+
+def x0fun():
+    x0 = [np.random.uniform(-1, 1), np.random.uniform(-1., 1.), 0, 0]
+    return x0
+
+robot = MarBot(cost, x0fun)
+t = 10
+dt = 0.02
+
+path = '../../../results/ddpg/marbot/experiment1/'
+
+algorithm = DDPG(robot, t, dt, path=path, warm_up=10000)
+algorithm.load()
+algorithm.run_learning(2000)
+
+
+x0 = [0., 0., 0.3, 0.]
+algorithm.run_controller(x0)
+algorithm.plot()
+algorithm.animation()
