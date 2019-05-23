6ad03b5c43c81a7bfcb02b4485db35c036127a88
mpritzkoleit <maxpritzkoleit@aol.com>
2019-05-23 23:23:51 +0200

    update example

--- examples/ddpg/cart_pole.py
+++ examples/ddpg/cart_pole.py
@@ -6,23 +6,23 @@ import numpy as np
 def cost(x_, u, x):
     x1, x2, x3, x4 = x
     u1, = u
-    c = 8.7*x1**2 + 8.7*x2**2 + 10e-5*x3**2 + 10e-5*x4**2 + 4.7*u1**2
+    c = x1**2 + x2**2 + 0.01*x3**2 + 0.01*x4**2 + 0.05*u1**2
     return c
 
 def x0fun():
-    x0 = [np.random.uniform(-0.1, 0.1), np.random.uniform(0.9*np.pi, 1.1*np.pi), 0, 0]
+    x0 = [np.random.uniform(-0.001, 0.001), np.random.uniform(0.999*np.pi, 1.001*np.pi), 0, 0]
     return x0
 
 t = 10
-dt = 0.02
+dt = 0.03
 
-cartPole = CartPole(cost, x0fun, dt)
+cartPole = CartPole(cost, x0fun, dt, terminal_cost=200)
 
-path = '../../../results/ddpg/experiment55/'
+path = '../../../results/ddpg/experiment1/'
 
-algorithm = DDPG(cartPole, t, dt, path=path, warm_up=100, plotInterval=1)
+algorithm = DDPG(cartPole, t, dt, path=path, warm_up=10000)
 algorithm.load()
-algorithm.run_learning(2000)
+algorithm.run_learning(1000)
 
 x0 = [0.0, np.pi, 0, 0]
 algorithm.run_controller(x0)
