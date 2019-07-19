bfea1fe6b939c70f8174ae44807e0a52b99b0023
pritzkoleit <maxpritzkoleit@aol.com>
2019-07-19 10:05:54 +0200

    update examples

--- examples/mbrl/test.py
+++ examples/mbrl/test.py
@@ -25,14 +25,14 @@ def p_x0():
 
 
 x0 = [0, np.pi, 0, 0]
-t = 6 # time of an episode
+t = 4 # time of an episode
 dt = 0.02 # time step-size
 
 env = CartPole(c_k, x0, dt)
 
 path = '../../../results/mbrl/'  # path, where results are saved
 
-rl_algorithm = MBRL(env, t, dt, path=path, warm_up=500, horizon=2.,  use_mpc_plan=False, ilqr_print=True) # instance of the DDPG algorithm
-
+rl_algorithm = MBRL(env, t, dt, path=path, warm_up=500, horizon=2.,  use_mpc_plan=True, ilqr_print=True) # instance of the DDPG algorithm
+#rl_algorithm.load()
 rl_algorithm.run_learning(1e6)
 
--- examples/nmpc/cart_pole.py
+++ examples/nmpc/cart_pole.py
@@ -7,21 +7,20 @@ import matplotlib.pyplot as plt
 def c_k(x, u):
     x1, x2, x3, x4 = x
     u1, = u
-    c = .5*(10.*x1**2 + 20*x2**2 + .01*x3**2 + .01*x4**2) + 0.01*u1**2
+    c = .5*(20.*x1**2 + 20*x2**2 + .01*x3**2 + .01*x4**2) + 0.01*u1**2
     return c
 
 x0 = [0, np.pi, 0, 0]
 
 t = 6.
-horizon = 2.
+horizon = 1.8
 dt = 0.02
 
 env = CartPole(c_k, x0, dt)
 mpc_env = CartPole(c_k, x0, dt)
- 
 
 path = '../../../results/nmpc/cart_pole/'
 
-algorithm = NMPC(env, mpc_env, t, dt, horizon, path=path)
+algorithm = NMPC(env, mpc_env, t, dt, horizon, path=path, ilqr_print=True, step_iterations=5)
 # reset environment/agent to initial state, delete history
 algorithm.run()
