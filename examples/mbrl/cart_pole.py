f2d755c3515af6a2f0e0d0bc201ded9688161938
mpritzkoleit <maxpritzkoleit@aol.com>
2019-12-09 17:54:48 +0100

    remove examples

--- examples/mbrl/cart_pole.py
+++ examples/mbrl/cart_pole.py
@@ -1,52 +1,53 @@
-from pygent.environments import Acrobot
-from pygent.algorithms.mbrl import MBRL
-
 import numpy as np
+import scipy.optimize as optim
+import argparse
 import matplotlib
 matplotlib.use('Agg') # disable interactive display of figures on the HPC-cluster
-# define the incremental cost
 
-import argparse
+# pygent imports
+from pygent.environments import CartPole
+from pygent.algorithms.mbrl import MBRL
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--time_step", type=float, default=0.03)
+parser.add_argument("--time_step", type=float, default=0.02)
 parser.add_argument("--use_mpc", type=int, default=0)
-parser.add_argument("--warm_up_episodes",type=int,  default=5)
+parser.add_argument("--warm_up_episodes",type=int,  default=3)
 parser.add_argument("--agg", type=int, default=1)
 parser.add_argument("--epochs", type=int, default=60)
 parser.add_argument("--weight_decay", type=float, default=5e-4)
-parser.add_argument("--data_noise", type=float, default=1e-3)
 parser.add_argument("--pred_err_bound", type=float, default=1e-2)
 args = parser.parse_args()
 
-# define the incremental cost
-# define the incremental cost
+# incremental cost
 def c_k(x, u):
     x1, x2, x3, x4 = x
     u1, = u
-    c = 5*x1**2 + x2**2 + 0.01*x3**2 + 0.01*x4**2 + 0.005*u1**2
+    c = 2*x1**2 + 5*x2**2 + .01*x3**2 + .01*x4**2 + 0.1*u1**2
     return c
 
-# define the final cost at step N
+# final cost at timestep N
 def c_N(x):
     x1, x2, x3, x4 = x
-    c = 100*x1**2 + 10*x2**2 + 10*x3**2 + 10*x4**2
+    c = 100*x1**2 + 100*x2**2 + 100*x3**2 + 100*x4**2
     return c
 
 # define the function, that represents the initial value distribution p(x_0)
 def p_x0():
-    x0 = [np.random.uniform(0.99*np.pi, 1.01*np.pi), np.random.uniform(-0.01, 0.01), 0, 0]
+    x0 = [np.random.uniform(-0.005, 0.005), np.pi, 0, 0]
     return x0
 
 
-t = 6 # time of an episode
+x0 = [0, np.pi, 0, 0] # initial value
+t = 5 # duration of an episode in seconds
 dt = args.time_step # time step-size
 
-env = Acrobot(c_k, p_x0, dt, linearized=False)
-env.uMax = env.uMax*0.7
+# initialize the system 
+sys = CartPole(c_k, p_x0, dt)
 
-path = '/scratch/p_da_reg/results/mbrl/acrobot/'+'mpc='+str(args.use_mpc)+'/'+'weight_decay='+str(args.weight_decay)+'/'
+path = '../results/mbrl/'  # path, where results are saved'
 
-rl_algorithm = MBRL(env, t, dt,
+# instance of the model-based RL algorithm 
+rl_algorithm = MBRL(sys, t, dt,
                     path=path,
                     horizon=2.,
                     fcost=c_N,
@@ -57,9 +58,7 @@ rl_algorithm = MBRL(env, t, dt,
                     aggregation_interval=args.agg,
                     training_epochs=args.epochs,
                     weight_decay=args.weight_decay,
-                    data_noise=args.data_noise,
-                    prediction_error_bound=args.pred_err_bound,
-                    maxIters=200)
+                    prediction_error_bound=args.pred_err_bound)
 
-rl_algorithm.run_learning(500)
+rl_algorithm.run_learning(50)
 
